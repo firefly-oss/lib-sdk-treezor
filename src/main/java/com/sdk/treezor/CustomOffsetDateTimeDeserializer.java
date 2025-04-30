a1dfd227a6d85454ebde11407567018fbb145bb0
Carlos Sánchez <casc84ab@gmail.com>
2025-04-30 09:53:29 +0200

    Handle zeros date in deserializer

--- src/main/java/com/sdk/treezor/CustomOffsetDateTimeDeserializer.java
+++ src/main/java/com/sdk/treezor/CustomOffsetDateTimeDeserializer.java
@@ -11,12 +11,18 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class CustomOffsetDateTimeDeserializer extends JsonDeserializer<OffsetDateTime> {
-    
+
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    
+
     @Override
     public OffsetDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         String dateString = p.getText();
+
+        // Handle the special case of '0000-00-00 00:00:00'
+        if (dateString.equals("0000-00-00 00:00:00")) {
+            return null; // Return null for invalid date
+        }
+
         try {
             // Parse the date string without timezone information
             LocalDateTime localDateTime = LocalDateTime.parse(dateString, formatter);
